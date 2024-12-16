8d46d515f8122348c3f794a24c1606af0091b19c
Faiz2979 <faizannabil877@gmail.com>
2024-12-16 21:48:59 +0700

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,5 @@
 'use client'
-import Link from "next/link";
 import { useEffect, useState } from "react";
-import handlePageChange from "./Movie";
     export default function Navbar() {
         const [scrollY, setScrollY] = useState(0);
         useEffect(() => {
@@ -20,13 +18,9 @@ import handlePageChange from "./Movie";
         <nav
             className={`px-[30px] lg:px-[100px] py-[5px] flex items-center justify-between h-[70px] transition-all fixed w-full duration-300 z-[999] overflow-hidden`}
         >
-            <Link
-            onClick={() => handlePageChange(1,"#top")}
-            href={"/"}
-            className="text-white z-10 text-lg lg:text-4xl oxanium oxanium-semibold leading-none inline-flex items-end gap-[2px]"
-            >
+            <div className="text-white z-10 text-lg lg:text-4xl oxanium oxanium-semibold leading-none inline-flex items-end gap-[2px]">
             TM<span className="text-red-600">DB</span><span className="w-2 h-2 rounded-full bg-red-600"></span>
-            </Link>
+            </div>
             <div
             className={`absolute w-full h-full left-0 top-0 z-0 transition-all duration-300 ${scrollY > 100? "bg-[#a10f0f] bg-opacity-20 backdrop-blur-sm": "bg-transparent"}`}
             ></div>
