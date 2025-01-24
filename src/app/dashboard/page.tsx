2b1254a1a77f7f4db92cc0ad74878e827d5149f3
Faiz2979 <faizannabil877@gmail.com>
2025-01-24 20:02:52 +0700

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,8 +2,16 @@
 
 export default function Home() {
     return(
-        <div className="w-full h-full flex items-center justify-center bg-white">
-            Dashboard
+        <div className="w-full h-full flex items-center justify-center oxanium mt-5 ">
+            <div>
+                <h1 className="text-4xl">Dashboard</h1>
+                <h2 className="text-2xl">
+                    Nothing Here yet Go To  
+                    <span> 
+                        <a className="text-red-600 text-2xl align-middle hover:underline underline-red-500" href="/movie"> Movie Page </a>
+                    </span> 
+                </h2>
+            </div>
         </div>
     )
 }
